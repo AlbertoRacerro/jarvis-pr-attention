38ace955e428275b8b0e142b93991d88c928fcdb
AlbertoRacerro <alberto.racerro@mail.polimi.it>
2026-08-29 10:23:31 +0200

    Export reviewer handoff helpers

--- src/pr_attention/__init__.py
+++ src/pr_attention/__init__.py
@@ -1,8 +1,16 @@
 """Deterministic exact-head pull request attention snapshots."""
 
 from .cli import collect_snapshot
+from .handoff import build_review_envelope, build_review_result_template
 from .packet import collect_review_packet
 from .review_result import packet_sha256, validate_review_result
 
-__all__ = ["collect_snapshot", "collect_review_packet", "packet_sha256", "validate_review_result"]
-__version__ = "0.4.0"
+__all__ = [
+    "collect_snapshot",
+    "collect_review_packet",
+    "packet_sha256",
+    "validate_review_result",
+    "build_review_result_template",
+    "build_review_envelope",
+]
+__version__ = "0.5.0"
