2f6267cdd6a2955ac57f66e3bd621a3d6df09a3d
AlbertoRacerro <alberto.racerro@mail.polimi.it>
2026-08-29 15:04:38 +0200

    Use complete V1.11 review-thread collection

--- src/pr_attention/rereview_packet.py
+++ src/pr_attention/rereview_packet.py
@@ -9,10 +9,10 @@ from .rereview_packet_v11 import (
     REREVIEW_PACKET_KIND,
     REREVIEW_PACKET_SCHEMA_VERSION,
     build_rereview_packet,
-    collect_rereview_packet,
     failed_checkpoint,
     rereview_packet_sha256,
 )
+from .rereview_threads_v11 import collect_rereview_packet
 
 __all__ = [
     "CONTENT_TRUST",
