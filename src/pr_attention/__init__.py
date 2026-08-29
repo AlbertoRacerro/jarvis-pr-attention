f7566f739e15019d1f2003a6401816ed9c959ae5
AlbertoRacerro <alberto.racerro@mail.polimi.it>
2026-08-29 12:04:10 +0200

    Export compaction metrics API

--- src/pr_attention/__init__.py
+++ src/pr_attention/__init__.py
@@ -5,6 +5,7 @@ from .compact import build_attention_digest, build_repair_packet
 from .evidence_bundle import build_evidence_bundle, bundle_sha256, verify_evidence_bundle
 from .handoff import build_review_envelope, build_review_result_template
 from .integration_gate import build_integration_gate
+from .metrics import measure_compaction
 from .packet import collect_review_packet
 from .review_result import packet_sha256, validate_review_result
 
@@ -21,5 +22,6 @@ __all__ = [
     "verify_evidence_bundle",
     "build_attention_digest",
     "build_repair_packet",
+    "measure_compaction",
 ]
-__version__ = "0.8.0"
+__version__ = "0.9.0"
